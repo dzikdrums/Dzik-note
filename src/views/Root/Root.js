aece73b860a5322468d7ad1b036431872e30b9a8
Maciej Dzik <dzikdrums@gmail.com>
2020-02-13 09:54:06 +0100

    add Button components

---
+++ src/views/Root/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+function Root() {
+  return (
+    <div>
+      <Button>Close/save</Button>
+      <Button secondary>Remove</Button>
+    </div>
+  );
+}
+
+export default Root;
