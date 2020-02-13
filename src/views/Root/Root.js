774236bf1ed488904aefafcdea937ba59ca3a8e6
Maciej Dzik <dzikdrums@gmail.com>
2020-02-13 10:16:04 +0100

    add global styles

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'Theme/GlobalStyle';
 
 function Root() {
   return (
     <div>
+      <GlobalStyle />
       <Button>Close/save</Button>
       <Button secondary>Remove</Button>
     </div>
